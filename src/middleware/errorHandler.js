1b30ac41e2a5604e75c4cda4010596894a4c670f
B-got-banned <onyealilachib0850@student.babcock.edu.ng>
2026-03-08 14:54:51 +0100

    Updated error handler to accommodate Multer errors better

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,8 +1,13 @@
+const multer = require("multer")
+
 const errorHandler = (error, req, res, next) => {
   console.error(error.message)
   console.error(error.stack || "")
   const status = error.status || 500
-  res.status(status).json({Error: error.message})
+  if(error instanceof multer.MulterError){
+    res.status(400).json({Error: "File upload error. File may be too large :("})
+  }
+  res.status(status).json({Error: error.message || "Something went wrong :("})
 }
 
 module.exports = errorHandler
