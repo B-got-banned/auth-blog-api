ce1e09f61f768907ccf5bc402824c334b4f33315
B-got-banned <onyealilachib0850@student.babcock.edu.ng>
2026-03-08 14:54:26 +0100

    Updated user model to accomodate file uploads

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,6 +4,7 @@ const userSchema = mongoose.Schema({
   name: {type: String, required: true},
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
+  profileImage: {type: String},
   refreshToken: {type: String, trim: true}
 }, {timestamps: true})
 
