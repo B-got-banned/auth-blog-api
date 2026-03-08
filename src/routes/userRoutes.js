6ae78ee1dcb644fd77282de601919da4d4e730e5
B-got-banned <onyealilachib0850@student.babcock.edu.ng>
2026-03-08 14:55:59 +0100

    Included endpoint for separate uploads (will be worked on) and updated post endpoint for files

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {signUserUp, logUserIn, refreshToken} = require('../controllers/userController')
+const {signUserUp, logUserIn, refreshToken, uploadImage} = require('../controllers/userController')
 const { validateSignUp, validateLogIn } = require('../validations/userValidation')
+const upload = require('../middleware/upload')
 
-router.post('/signup', validateSignUp, signUserUp)
+router.post('/signup', upload.single("image"), validateSignUp, signUserUp)
 router.post('/login', validateLogIn, logUserIn)
 router.post('/refresh-token', refreshToken)
+router.post('/upload', upload.single("image"), uploadImage) //will be changed for future updates
 
 module.exports = router
